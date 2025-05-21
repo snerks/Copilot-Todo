30c68c5fb774e711afad894fa2ae005bdb5a3b15
snerks <martin.github@outlook.com>
2025-05-21 22:47:46 +0100

    Fix Base URL Casing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/copilot-todo/",
+  base: "/Copilot-Todo/",
 
   build: {
     sourcemap: true,
